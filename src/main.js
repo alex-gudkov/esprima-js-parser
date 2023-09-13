cea8a86c5c212ba1524f7bc6fc53ccee8cd7f7b9
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-13 17:49:19 +0300

    move JS source to src/ folder

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ var esprima = require('esprima');
 
 function jsToTokens(inputFile, outputFile) {
   // read JS file
-  var inputFilePath = path.join(__dirname, inputFile);
+  var inputFilePath = path.join(__dirname, '..', inputFile);
   var inputFileData = fs.readFileSync(inputFilePath, { encoding: 'utf8' });
 
   // tokenize JS file
   var tokens = esprima.tokenize(inputFileData);
 
   // write tokens file
-  var outputFilePath = path.join(__dirname, outputFile);
+  var outputFilePath = path.join(__dirname, '..', outputFile);
   var outputFileData = JSON.stringify(tokens, null, 2) + '\n';
 
   fs.writeFileSync(outputFilePath, outputFileData);
@@ -19,14 +19,14 @@ function jsToTokens(inputFile, outputFile) {
 
 function jsToAst(inputFile, outputFile) {
   // read JS file
-  var inputFilePath = path.join(__dirname, inputFile);
+  var inputFilePath = path.join(__dirname, '..', inputFile);
   var inputFileData = fs.readFileSync(inputFilePath, { encoding: 'utf8' });
 
   // parse JS file
   var abstractSyntaxTree = esprima.parse(inputFileData);
 
   // write AST file
-  var outputFilePath = path.join(__dirname, outputFile);
+  var outputFilePath = path.join(__dirname, '..', outputFile);
   var outputFileData = JSON.stringify(abstractSyntaxTree, null, 2) + '\n';
 
   fs.writeFileSync(outputFilePath, outputFileData);
